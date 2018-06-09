e594b1885da1bccc58c087f98d26eaade9bad7f5
pradumangoyal <goyal.praduman611@gmail.com>
2018-06-09 18:56:16 +0530

    Added a Simple Signup Form

---
+++ backend/stream/stream/views.py
@@ -0,0 +1,17 @@
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('localhost:8000/admin')
+    else:
+        form = UserCreationForm()
+    return render(request, 'stream/signup.html', {'form': form})
