ac37f619fa44577961d3f0911bacd1e33f7c2bdb
pradumangoyal <goyal.praduman611@gmail.com>
2018-06-09 22:38:08 +0530

    Login form added, is_active in functioning, a home page which check admin or normal user

--- backend/stream/stream/urls.py
+++ backend/stream/stream/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('signup/', views.signup, name='signup'),
+    path('login/', auth_views.login,{'template_name': 'stream/login.html'}, name='login'),
+    path('logout/', auth_views.logout, name='logout'),
+    path('', views.home, name="home"),
+
 ]
--- backend/stream/stream/views.py
+++ backend/stream/stream/views.py
@@ -1,6 +1,9 @@
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+
 
 def signup(request):
     if request.method == 'POST':
@@ -9,9 +12,17 @@ def signup(request):
             form.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('localhost:8000/admin')
+            user = User.objects.get(username=username)
+            user.is_active = False
+            user.save()
+            return redirect('../login')
     else:
         form = UserCreationForm()
     return render(request, 'stream/signup.html', {'form': form})
+
+@login_required(login_url='login')
+def home(request):
+    if(request.user.is_superuser):
+        return render(request, 'stream/home.html', {'message': 'admin ki jai'})
+    else:
+        return render(request, 'stream/home.html')
